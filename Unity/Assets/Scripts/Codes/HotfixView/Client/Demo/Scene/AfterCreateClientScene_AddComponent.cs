724a42f34c50f5dafa27dc2c595df8ac68da254b
HLM2 <492701698@qq.com>
2024-02-05 16:25:24 +0800

    新建项目

---
+++ Unity/Assets/Scripts/Codes/HotfixView/Client/Demo/Scene/AfterCreateClientScene_AddComponent.cs
@@ -0,0 +1,16 @@
+namespace ET.Client
+{
+    [Event(SceneType.Client)]
+    public class AfterCreateClientScene_AddComponent: AEvent<EventType.AfterCreateClientScene>
+    {
+        protected override async ETTask Run(Scene scene, EventType.AfterCreateClientScene args)
+        {
+            scene.AddComponent<UIEventComponent>();
+            scene.AddComponent<UIPathComponent>();
+            scene.AddComponent<UIComponent>();
+            scene.AddComponent<ResourcesLoaderComponent>();
+            
+            await ETTask.CompletedTask;
+        }
+    }
+}
